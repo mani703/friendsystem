cdc8dffbb431768e4fc48ae8edefa6f5a7e66b4e
mani703 <mani703@naver.com>
2025-04-07 22:34:40 +0900

    study lobbok

--- src/main/java/com/example/javaallinone/project4/friendsystem/domain/Person.java
+++ src/main/java/com/example/javaallinone/project4/friendsystem/domain/Person.java
@@ -3,8 +3,16 @@ package com.example.javaallinone.project4.friendsystem.domain;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDate;
 
 @Entity
+@Getter
+@Setter
+@ToString
 public class Person {
 
     @Id
@@ -15,36 +23,17 @@ public class Person {
 
     private int age;
 
-    public Long getId() {
-        return id;
-    }
+    private String hobby;
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+    private String bloodType;
 
-    public String getName() {
-        return name;
-    }
+    private String address;
 
-    public void setName(String name) {
-        this.name = name;
-    }
+    private LocalDate birthday;
 
-    public int getAge() {
-        return age;
-    }
+    private String job;
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+    @ToString.Exclude
+    private String phoneNumber;
 
-    @Override
-    public String toString() {
-        return "Person{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
 }
